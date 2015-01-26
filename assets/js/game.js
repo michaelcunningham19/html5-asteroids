5b3a7b68fa1ff438f63ebad1447679e0f03934a3
Michael-Cunningham <W0260053@nscc.ca>
2015-01-26 14:31:13 -0400

    Added CreateJS Library and BG Music

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,21 +4,41 @@ $(function () {
 	function construct() {
 
 		// declare variables
-		var game, context;
+		var game, context, frameRate, background;
 
 		// set the variables
-		game    = document.getElementById('game');
-		context = game.getContext('2d');
+		game      = document.getElementById('game');
+		context   = game.getContext('2d');
+        frameRate = 24;
 
 		// set the game's properties
-		context.fillStyle = '#000';
-		context.fillRect(0, 0, window.innerWidth, window.innerHeight);
+		game.width  = window.innerWidth;
+		game.height = window.innerHeight;
 		
+		background = new Image();
+		background.src = 'assets/gfx/background/layer1.png';
+
+		// Make sure the image is loaded first otherwise nothing will draw.
+		background.onload = function () {
+			context.drawImage(background, 0, 0);
+		};
+
 		// apply parallax effect to background
-		$('.parallax-layer').parallax({
-			mouseport: $('#background')
-		});
+		//$('.parallax-layer').parallax({
+		//	mouseport: $('#background')
+		//});
 		
+        function handleFileLoad(e) {
+            // A sound has been preloaded.
+            createjs.Sound.play(e.src);
+        }
+        
+        // Firefox and Opera will load "music.ogg"
+        // Browsers with MP3 support will load "music.mp3"
+        createjs.Sound.alternateExtensions = ['mp3'];
+        createjs.Sound.registerSound({id: 'backgroundMusic', src: 'assets/sfx/music.mp3'});
+        createjs.Sound.addEventListener('fileload', handleFileLoad);
+        
 	}
 	
 	construct();
